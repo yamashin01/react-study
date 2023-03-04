52dd5ed6a24b3f86058629a1a5f47fe9d5a33269
yamada <shinya.yamada@benext-corp.com>
2023-03-04 10:44:38 +0900

    ✨ add styled-jsx

--- css-modules/src/App.jsx
+++ css-modules/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyles";
+import { StyledJsx } from "./components/StyledJsx";
 
 function App() {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
