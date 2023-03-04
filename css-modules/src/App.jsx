bfb8811527967a71efa8d0c89f055dc00f15c013
yamada <shinya.yamada@benext-corp.com>
2023-03-04 10:54:58 +0900

    ✨ add emotion

--- css-modules/src/App.jsx
+++ css-modules/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { CssModules } from "./components/CssModules";
+import { Emotion } from "./components/Emotion";
 import { InlineStyle } from "./components/InlineStyles";
 import { StyledComponents } from "./components/StyledComponents";
 import { StyledJsx } from "./components/StyledJsx";
@@ -11,6 +12,7 @@ function App() {
       <CssModules />
       <StyledJsx />
       <StyledComponents />
+      <Emotion />
     </div>
   );
 }
