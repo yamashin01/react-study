d611725674676a0476a644b3ab6f5861d405e875
yamada <shinya.yamada@benext-corp.com>
2023-03-04 10:38:31 +0900

    ✨ add css-modules style

--- css-modules/src/App.jsx
+++ css-modules/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyles";
 
 function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
