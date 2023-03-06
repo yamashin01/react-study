6ee4e739dc7119a841b02c4fc79f41b81521273f
yamada <shinya.yamada@benext-corp.com>
2023-03-06 20:35:31 +0900

    ✨ create organism components

--- atomic-design/src/App.jsx
+++ atomic-design/src/App.jsx
@@ -1,14 +1,27 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SeachInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
 export default function App() {
+  const user = {
+    name: "taro",
+    imageUrl:
+      "https://images.unsplash.com/photo-1535930891776-0c2dfb7fda1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+    email: "test@XXXX.com",
+    phone: "090-0000-1111",
+    company: {
+      name: "テスト株式会社",
+      website: "https://google.com",
+    },
+  };
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>テスト</SecondaryButton>
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
