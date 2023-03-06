628e53d84c231272b58326d0798a41ad36add594
yamada <shinya.yamada@benext-corp.com>
2023-03-06 16:12:12 +0900

    ✨ add useNavigate

--- react-router/src/Page1.jsx
+++ react-router/src/Page1.jsx
@@ -1,7 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
+
+  const navigate = useNavigate();
+  const onClickDetailA = () => navigate("/page1/DetailA");
   return (
     <div>
       <h1>Page1ページです</h1>
@@ -10,6 +13,8 @@ export const Page1 = () => {
       </Link>
       <br />
       <Link to="/page1/DetailB">DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
--- react-router/src/Page1DetailA.jsx
+++ react-router/src/Page1DetailA.jsx
@@ -1,11 +1,15 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const { state } = useLocation();
+  const navigate = useNavigate();
+
+  const onClickBack = () => navigate(-1);
   console.log(state);
   return (
     <div>
       <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
