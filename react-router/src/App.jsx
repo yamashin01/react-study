ad3110ec8d7802ecf5061424645fd7ad12322c5a
yamada <shinya.yamada@benext-corp.com>
2023-03-06 15:07:49 +0900

    🎉 study react router project

---
+++ react-router/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
