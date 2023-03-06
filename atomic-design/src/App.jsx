cb7cd0aa41e9c25f1644ccbad657492d56e61bf0
yamada <shinya.yamada@benext-corp.com>
2023-03-06 21:38:25 +0900

    ✨ create page components

--- atomic-design/src/App.jsx
+++ atomic-design/src/App.jsx
@@ -1,33 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SeachInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
-  const user = {
-    name: "taro",
-    imageUrl:
-      "https://images.unsplash.com/photo-1535930891776-0c2dfb7fda1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-    email: "test@XXXX.com",
-    phone: "090-0000-1111",
-    company: {
-      name: "テスト株式会社",
-      website: "https://google.com",
-    },
-  };
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <DefaultLayout>
-          <PrimaryButton>テスト</PrimaryButton>
-          <SecondaryButton>検索</SecondaryButton>
-          <SearchInput />
-          <UserCard user={user} />
-        </DefaultLayout>
-      </BrowserRouter>
-    </div>
-  );
+  return <Router />;
 }
---
+++ atomic-design/src/components/pages/Users.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SeachInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `taro${val}`,
+    imageUrl:
+      "https://images.unsplash.com/photo-1535930891776-0c2dfb7fda1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+    email: "test@XXXX.com",
+    phone: "090-0000-1111",
+    company: {
+      name: "テスト株式会社",
+      website: "https://google.com",
+    },
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <SUserArea>
+        <h2>ユーザーページです。</h2>
+        <SearchInput />
+        {users.map((user) => {
+          return <UserCard key={user.id} user={user} />;
+        })}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 48px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
