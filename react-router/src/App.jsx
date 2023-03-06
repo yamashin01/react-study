e4bf3d347cd1cf91947801fb436745c781a0d7e4
yamada <shinya.yamada@benext-corp.com>
2023-03-06 15:25:52 +0900

    ✨ add nest pages of Page1

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -17,6 +19,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/page1" element={<Page1 />} />
+        <Route path="/page1/DetailA" element={<Page1DetailA />} />
+        <Route path="/page1/DetailB" element={<Page1DetailB />} />
         <Route path="/page2" element={<Page2 />} />
       </Routes>
     </BrowserRouter>
