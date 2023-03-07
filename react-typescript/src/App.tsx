60a9385223747416c24ea0646209fa9d17c3be83
yamada <shinya.yamada@benext-corp.com>
2023-03-07 17:14:14 +0900

    ✨ add PropsType

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -22,7 +22,13 @@ function App() {
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => {
-        return <Todo title={todo.title} userId={todo.userId} />;
+        return (
+          <Todo
+            title={todo.title}
+            userId={todo.userId}
+            completed={todo.completed}
+          />
+        );
       })}
     </div>
   );
--- react-typescript/src/Todo.tsx
+++ react-typescript/src/Todo.tsx
@@ -1,4 +1,11 @@
-export const Todo = (props) => {
-  const { title, userId } = props;
-  return <p>{`${title}(ユーザー：${userId})`}</p>;
+type PropsType = {
+  userId: number;
+  title: string;
+  completed?: boolean;
+};
+
+export const Todo = (props: PropsType) => {
+  const { title, userId, completed = false } = props;
+  const isCompletedStr: string = completed ? "[完]" : "[未]";
+  return <p>{`${isCompletedStr}${title}(ユーザー：${userId})`}</p>;
 };
