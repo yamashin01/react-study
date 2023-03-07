a1c7828ed429f305ad6b78da19d5f4324e8aeb05
yamada <shinya.yamada@benext-corp.com>
2023-03-07 17:24:58 +0900

    ♻️ add FC

--- react-typescript/src/Todo.tsx
+++ react-typescript/src/Todo.tsx
@@ -1,6 +1,7 @@
+import { FC } from "react";
 import { TodoType } from "./types/todo";
 
-export const Todo = (props: Omit<TodoType, "id">) => {
+export const Todo: FC<Omit<TodoType, "id">> = (props) => {
   const { title, userId, completed = false } = props;
   const isCompletedStr: string = completed ? "[完]" : "[未]";
   return <p>{`${isCompletedStr}${title}(ユーザー：${userId})`}</p>;
