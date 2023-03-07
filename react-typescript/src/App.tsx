a76efbd47ca13cdbe8841acca411821ff8aa4f85
yamada <shinya.yamada@benext-corp.com>
2023-03-07 17:08:10 +0900

    ✨ add TodoType

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import "./App.css";
-import { Practice1 } from "./practices/Practice1";
-import { Practice2 } from "./practices/Practice2";
-import { Practice3 } from "./practices/Practice3";
-import { Practice4 } from "./practices/Practice4";
+import { Todo } from "./Todo";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 function App() {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const onClickFetchData = () => {
+    axios
+      .get<TodoType[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
   return (
     <div className="App">
-      <Practice1 />
-      <Practice2 />
-      <Practice3 />
-      <Practice4 />
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => {
+        return <Todo title={todo.title} userId={todo.userId} />;
+      })}
     </div>
   );
 }
